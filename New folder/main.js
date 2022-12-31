195b270d027c1acd6550a9e8e7cde0b2785cc805
neetutiwa <110733241+neetutiwa@users.noreply.github.com>
2022-12-31 19:54:25 +0530

    Add files via upload

---
+++ New folder/main.js
@@ -0,0 +1,8 @@
+function startClassification()
+{
+    navigator.mediaDevices.getUserMedia({ audio: true});
+    classifier = m15.soundClassifier('https://teachablemachine.withgoogle.com/models/kA4F-80yW/model.json', modelReady);
+}
+function modelReady(){
+    classifier.classify(gotResults);
+}
